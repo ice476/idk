479993655523f45f994aeedb21f8699d26b7efdc
TonNom <ton@email.com>
2026-06-28 16:57:13 +0200

    Modification du bot

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
+from discord.ext import tasks
 from discord.ext import commands
 load_dotenv()
 
@@ -111,21 +112,41 @@ async def on_member_join(member):
 
 
 
-@bot.tree.command(name="staff", description="Demande de staff")
-async def staff(interaction: discord.Interaction, member: discord.Member):
-    embed = discord.Embed(
-        title="Rejoignez l'équipe de T-Shirt",
-        description="T-Shirt recrute du personnel. voici les conditions :",
-        color=discord.Color.blue()
-    )
-    embed.add_field(name="Age minimum", value="pour devenir staff minimum 15ans", inline=False)
-    embed.add_field(name="Activité", value="5h de vocal ou 500 messages", inline=False)
-    embed.add_field(name="Clean", value="Pour finir faut être clean 0 sanction", inline=False)
-    embed.add_field(name="", value="Si vous souhaitez devenir Staff, contactez une personne haut gradée.", inline=False)
-    embed.set_footer(text="Cordialement")
-    embed.set_image(url="https://i.pinimg.com/webp85/1200x/a8/84/a8/a884a8c972360381071e972f1a6b659e.webp")
+@tasks.loop(hours=720)
+async def message_recrutement_mensuel():
+    ID_SALON_STAFF = 1518366776952623357
+    channel = bot.get_channel(ID_SALON_STAFF)
+    
+    if channel:
+        embed = discord.Embed(
+            title="Rejoignez l'équipe de T-Shirt",
+            description="T-Shirt recrute du personnel. Voici les conditions :",
+            color=discord.Color.blue()
+        )
+        embed.add_field(name="Age minimum", value="Pour devenir staff minimum 15 ans", inline=False)
+        embed.add_field(name="Activité", value="5h de vocal ou 500 messages", inline=False)
+        embed.add_field(name="Clean", value="Pour finir faut être clean 0 sanction", inline=False)
+        embed.add_field(name="Candidature", value="Si vous souhaitez devenir Staff, contactez une personne haut gradée.", inline=False)
+        embed.set_footer(text="Cordialement")
+        embed.set_image(url="https://i.pinimg.com/webp85/1200x/a8/84/a8/a884a8c972360381071e972f1a6b659e.webp")
 
-    await interaction.response.send_message(embed=embed)
+        await channel.send(content="@everyone", embed=embed)
+
+@bot.event
+async def on_ready():
+    if not message_recrutement_mensuel.is_running():
+        message_recrutement_mensuel.start()
+    print(f"Connecté en tant que {bot.user}")
+
+
+@bot.event
+async def on_member_join(member):
+    ID_SALON_STAFF = 1518366776952623357
+    channel = bot.get_channel(ID_SALON_STAFF)
+    
+    if channel:
+        await channel.send(f"Hey {member.mention} ! Regarde les conditions ci-dessus si tu veux rejoindre le staff !")
+
 
 @bot.tree.command(name="warnguy", description="Alert une personne")
 async def warnguy(interaction: discord.Interaction, member: discord.Member):
