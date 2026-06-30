8fa837637a4fff23d23eb6a7d0de72c18b053baa
TonNom <ton@email.com>
2026-06-30 20:43:00 +0200

    Modification du bot

--- bot.py
+++ bot.py
@@ -434,3 +434,5 @@ async def youtube(interaction: discord.Interaction):
     await interaction.response.send_message(
         "Voici le lien de ma chaine : https://www.youtube.com/@Nawkini"
     )
+    
+    bot.run(os.getenv("DISCORD_TOKEN"))
